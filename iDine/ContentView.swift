c9b06d2b2be13e08348c3ce067ef066040d6ff0e
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-04-23 09:21:48 -0500

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+// iDine
+// Copyright ©️ 2024 Iván Camilo Fuertes. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,14 @@
+// iDine
+// Copyright ©️ 2024 Iván Camilo Fuertes. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
