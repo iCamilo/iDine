f807cf4c53670d8587f984b6b8072a7edd0364ce
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-04-23 11:57:38 -0500

    Style ItremRow to show the name in a bigger font and clip shape the thumbnail image

--- iDine/Views/ItemRow.swift
+++ iDine/Views/ItemRow.swift
@@ -10,8 +10,11 @@ struct ItemRow: View {
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
             }
         }
