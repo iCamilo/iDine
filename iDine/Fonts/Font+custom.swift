965dbd5b9893413172babe0c49e41dc7c08c8287
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-04-25 09:30:54 -0500

    Add custom font to project

---
+++ iDine/Fonts/Font+custom.swift
@@ -0,0 +1,11 @@
+// iDine
+// Copyright ©️ 2024 Iván Camilo Fuertes. All rights reserved.
+
+
+import SwiftUI
+
+extension Font {
+    static func permanentMarkerRegular(size: CGFloat = 15) -> Font {
+        .custom("PermanentMarker-Regular", size: size)
+    }
+}
